d64942133b00d39514df84bab190138043879354
Dalton Scharff <dalton.scharff@gmail.com>
2023-02-16 08:39:28 -0600

    modify findAllKits to accept limit, offset, and labelId query parameters

--- src/pages/api/kits/index.ts
+++ src/pages/api/kits/index.ts
@@ -1,11 +1,41 @@
 import { Kit } from "@/interfaces/kit";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { findAllKits } from "@/services/kitService";
+import { HttpError } from "@/interfaces/httpError";
+
+const MAX_LIMIT = 25;
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Kit[]>
+  res: NextApiResponse<Kit[] | HttpError>
 ) {
-  const kits = findAllKits();
+  const { labelId, limit, offset } = req.query;
+  const labelIdString = Array.isArray(labelId) ? labelId[0] : labelId;
+  let limitNumber = parseIntegerFromQueryParam(limit);
+  const offsetNumber = parseIntegerFromQueryParam(offset);
+
+  if (limitNumber && (isNaN(limitNumber) || limitNumber < 1))
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "`limit` must be a positive number greater than 0",
+    });
+  if (offsetNumber && (isNaN(offsetNumber) || offsetNumber < 0))
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "`offset` must be a positive number",
+    });
+
+  if (limitNumber && limitNumber > MAX_LIMIT) limitNumber = MAX_LIMIT;
+
+  const kits = findAllKits({
+    labelId: labelIdString,
+    limit: limitNumber,
+    offset: offsetNumber,
+  });
   res.status(200).json(kits);
+}
+
+function parseIntegerFromQueryParam(value: string | string[] | undefined) {
+  if (value === undefined) return undefined;
+  return parseInt(Array.isArray(value) ? value[0] : value);
 }
--- src/services/kitService.ts
+++ src/services/kitService.ts
@@ -2,8 +2,25 @@ import { Kit } from "@/interfaces/kit";
 
 const kits: Kit[] = require("../../data/KITS_SHIPPING_DATA");
 
-export function findAllKits() {
-  return kits;
+interface SearchOptions {
+  labelId: string;
+  limit: number;
+  offset: number;
+}
+
+export function findAllKits(opts?: Partial<SearchOptions>) {
+  const options: SearchOptions = {
+    labelId: opts?.labelId ?? "",
+    limit: opts?.limit ?? 10,
+    offset: opts?.offset ?? 0,
+  };
+  return kits
+    .filter((kit) => {
+      const strippedKitLabelId = kit.label_id.replaceAll("-", "");
+      const strippedSearchLabelId = options.labelId.replaceAll("-", "");
+      return strippedKitLabelId.startsWith(strippedSearchLabelId);
+    })
+    .slice(options.offset, options.offset + options.limit);
 }
 
 export function findKitById(id: Kit["id"]) {
