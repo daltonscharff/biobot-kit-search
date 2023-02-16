b8bfe6cfd90948f4da3b515115c1b29dd7c7c9f6
Dalton Scharff <dalton.scharff@gmail.com>
2023-02-16 13:26:45 -0600

    add search field with autocomplete

---
+++ src/components/SearchField.tsx
@@ -0,0 +1,66 @@
+import { Kit, KitResponse } from "@/interfaces/kit";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { ChangeEvent, useEffect, useState } from "react";
+
+type Props = {
+  searchValue: string;
+  setSearchValue: (search: string) => void;
+};
+
+export default function SearchField({ searchValue, setSearchValue }: Props) {
+  const [autocompleteSuggestions, setAutocompleteSuggestions] = useState<
+    Kit["label_id"][]
+  >([]);
+  const autocompleteSearchParams = new URLSearchParams({
+    limit: "5",
+  });
+
+  const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+    if (e.target.value.length < 1) return;
+
+    autocompleteSearchParams.set("labelId", e.target.value);
+    const suggestionResponse = await fetch(
+      `/api/kits?${autocompleteSearchParams.toString()}`
+    );
+    const suggestionJson = (await suggestionResponse.json()) as KitResponse;
+    const suggestedLabelIds = suggestionJson.kits.map((kit) => kit.label_id);
+    setAutocompleteSuggestions(suggestedLabelIds);
+  };
+
+  useEffect(() => {
+    if (searchValue.length === 0) {
+      setAutocompleteSuggestions([]);
+    }
+  }, [searchValue]);
+
+  return (
+    <fieldset className="w-full space-y-1 text-gray-800">
+      <div className="relative">
+        <span className="absolute inset-y-0 right-0 flex items-center pr-2">
+          <button title="Search" className="p-1 focus:outline-none focus:ring">
+            <MagnifyingGlassIcon className="h-5" />
+          </button>
+        </span>
+        <div className="flex flex-col w-auto md:w-80">
+          <input
+            type="search"
+            list="searchFieldDatalist"
+            name="id"
+            placeholder="Kit Label ID"
+            className="py-2 pl-2 pr-10 text-sm rounded-sm focus:outline-none bg-gray-100 text-gray-800 focus:bg-gray-50 focus:border-blue-600 border"
+            value={searchValue}
+            onChange={onChange}
+          />
+          <datalist id="searchFieldDatalist">
+            {autocompleteSuggestions.map((suggestion, i) => (
+              <option key={`${suggestion}_${i}`} value={suggestion}>
+                {suggestion}
+              </option>
+            ))}
+          </datalist>
+        </div>
+      </div>
+    </fieldset>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import SearchField from "@/components/SearchField";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+  const onSearch = () => {
+    console.log("search for", searchValue);
+    setSearchValue("");
+  };
+
   return (
     <>
       <Head>
@@ -15,26 +22,18 @@ export default function Home() {
       </Head>
       <main className="flex items-center flex-col p-2 md:p-4 gap-4">
         <h1 className="text-3xl font-bold">Biobot Kit Search</h1>
-        <form>
-          <fieldset className="w-full space-y-1 text-gray-800">
-            <div className="relative">
-              <span className="absolute inset-y-0 right-0 flex items-center pr-2">
-                <button
-                  title="Search"
-                  className="p-1 focus:outline-none focus:ring"
-                >
-                  <MagnifyingGlassIcon className="h-5" />
-                </button>
-              </span>
-              <input
-                type="search"
-                name="id"
-                placeholder="Kit Label ID"
-                className="md:w-72 py-2 pl-2 pr-10 text-sm rounded-sm w-auto focus:outline-none bg-gray-100 text-gray-800 focus:bg-gray-50 focus:border-blue-600 border"
-              />
-            </div>
-          </fieldset>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSearch();
+          }}
+        >
+          <SearchField
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
         </form>
+        <div>other stuff</div>
       </main>
     </>
   );
